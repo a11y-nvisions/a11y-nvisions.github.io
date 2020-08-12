f94c33b079e709b0cc2e9621ad5aaa6811234a8d
ungjinpark <ungjin312@kakao.com>
2020-08-12 14:14:57 +0900

    upload React Project

---
+++ Agust_2020/precache-manifest.006c9a5995bc428c924fdc4d1d2ce44b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2baa227c50b4c6b4cf2564057a69bb92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18c438c719172a0e0af7",
+    "url": "/static/css/main.e389f20f.chunk.css"
+  },
+  {
+    "revision": "f53f0abc22f07439ee64",
+    "url": "/static/js/2.ed1b6d2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ed1b6d2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c438c719172a0e0af7",
+    "url": "/static/js/main.072407a8.chunk.js"
+  },
+  {
+    "revision": "02ed06d1e0901c17586e",
+    "url": "/static/js/runtime-main.2830ad60.js"
+  }
+]);
