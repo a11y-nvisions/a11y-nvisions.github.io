41339d347f54e2d59e37e044f90fc99229d4ed3f
ungjinpark <ungjin312@kakao.com>
2020-08-12 14:34:35 +0900

    test Agust_2020

--- Agust_2020/service-worker.js
+++ Agust_2020/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Agust_2020/precache-manifest.066082cc7dd9dea0adb61598f6d37a82.js"
+  "./Agust_2020/precache-manifest.d81658c24874601afa5d70347cf2119a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Agust_2020/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./Agust_2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
