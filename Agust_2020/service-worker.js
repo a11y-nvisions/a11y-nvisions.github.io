d112a3e17bd344a19ab377622f331a41e8ad408c
ungjinpark <ungjin312@kakao.com>
2020-08-12 14:28:42 +0900

    Test react App

--- Agust_2020/service-worker.js
+++ Agust_2020/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be4f4e7f3e434f194d90aa02b1cb42bf.js"
+  "/Agust_2020/precache-manifest.066082cc7dd9dea0adb61598f6d37a82.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Agust_2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
