0d04a1faa6f059bce0d259bbc97d5692130aee85
ungjinpark <ungjin312@kakao.com>
2020-09-28 17:24:17 +0900

    Add a  new page and videos For article

---
+++ September_2020/precache-manifest.1027cc97496ee705c5e82e3ab05a2175.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ee30f2fa387f9002e918afde41fc1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49c1bba7357e4abfa6da",
+    "url": "/static/css/main.823d997c.chunk.css"
+  },
+  {
+    "revision": "23c05d7872bbf8d0e9ce",
+    "url": "/static/js/2.a2e87082.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2e87082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49c1bba7357e4abfa6da",
+    "url": "/static/js/main.e92e12a0.chunk.js"
+  },
+  {
+    "revision": "d09614a53ea9868d45fa",
+    "url": "/static/js/runtime-main.03ced6e5.js"
+  }
+]);
