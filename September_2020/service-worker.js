d983d56048e50251604ae13f9dac06f6eb39619b
ungjinpark <ungjin312@kakao.com>
2020-09-28 17:30:32 +0900

    Fix a path for article page

--- September_2020/service-worker.js
+++ September_2020/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1027cc97496ee705c5e82e3ab05a2175.js"
+  "./precache-manifest.858905570736e9a7a37a25e2e2d3256c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
