c49934565c1b667856593d0d109e15ffb69dc489
Hyongsop Kim <hyongsop.kim@nvisions.co.kr>
2021-11-17 18:17:45 +0900

    announceForAccessibility 스크립트 추가

---
+++ js/announceForAccessibility.js
@@ -0,0 +1,54 @@
+/**
+ * Jquery 라이브러리 최신 버전 호출
+ * 이미 최신 버전이 존재할 경우 호출하지 않음
+ */
+var headScript = document.getElementsByTagName("head")[0].getElementsByTagName("script");
+for (var i = 0, check = false; i < headScript.length; i++){
+  if ("https://code.jquery.com/jquery-latest.min.js" === headScript[i].src){
+    check = true;
+    break;
+  }
+}
+if (!check){
+  var oScript = document.createElement("script");
+  oScript.type = "text/javascript";
+  oScript.charset = "utf-8";
+  oScript.src = "https://code.jquery.com/jquery-latest.min.js";
+  document.getElementsByTagName("head")[0].appendChild(oScript);
+}
+
+window.addEventListener('load', function() {
+  var btns = document.querySelectorAll('[screen-reader-live]');
+  btns.forEach(function (btn) {
+    btn.addEventListener('click', function() {
+      announceForAccessibility(btn.textContent);
+    });
+  });
+});
+
+/**
+ * div 안 p 태그에 메시지 삽입 후 body 끝에 추가하며 2초 후 삭제하는 함수
+ * div 의 name 은 div_announceForAccessibility 를 사용함 (해당 태그를 삭제하기 위함)
+ */
+function announceForAccessibility(message) {
+  var html = '' +
+    '<div aria-live="polite" name="div_announceForAccessibility" style="border: 0; padding: 0; margin: 0; ' +
+    'position: absolute !important;' + 'height: 1px; width: 1px; overflow: hidden; clip: rect(1px 1px 1px 1px); ' +
+    'clip: rect(1px, 1px, 1px, 1px);' + 'clip-path: inset(50%); white-space: nowrap;">' +
+    '<p name="p_announceForAccessibility"></p></div>';
+
+  $("body").append(html); // body 끝에 div_announceForAccessibility 추가
+
+  setTimeout(function () { // 0.3초 후 p 태그에 message 추가
+    $("[name='p_announceForAccessibility']").text(message);
+  }, 300);
+
+  setTimeout(removeAnnounceForAccessibility, 2000); // 2초 후 div_announceForAccessibility 삭제
+}
+
+/**
+ * div 의 name 이 div_announceForAccessibility 인 태그를 하위까지 삭제
+ */
+function removeAnnounceForAccessibility() {
+  $("[name='div_announceForAccessibility']").remove();
+}
