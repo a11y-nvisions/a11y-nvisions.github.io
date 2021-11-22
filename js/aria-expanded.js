7618d9797b2cbcd31cf60202fbdea29ba0be4d85
Hyongsop Kim <hyongsop.kim@nvisions.co.kr>
2021-11-22 17:32:25 +0900

    널리에서 개발한 aria-expanded.js 업데이트

---
+++ js/aria-expanded.js
@@ -0,0 +1,33 @@
+var expandButtons = document.querySelectorAll('[role="button"][aria-expanded], button[aria-expanded]');
+expandButtons.forEach(function (expandButton) {
+    expandedEvent(expandButton);
+});
+
+function expandedEvent(btn) {
+	if (btn.expandEvent) {
+		return false;
+	}
+	btn.expandEvent = true;
+	btn.addEventListener("click", function () {
+		var expandEl = document.querySelector("#" + btn.getAttribute("aria-controls"));
+		if (!expandEl) return;
+
+		expandButtons.forEach(function (expandButton) {
+			var ariaControls = expandButton.getAttribute("aria-controls")
+			var expandEls = document.querySelector("#" + ariaControls);
+			if (window.getComputedStyle(expandEls).display === 'none')
+				expandButton.setAttribute("aria-expanded", false);
+		});
+		if (btn.getAttribute("aria-expanded") === 'true' && window.getComputedStyle(expandEl).display === 'none') {
+			expandedClose(btn, expandEl);
+		} else if (btn.getAttribute("aria-expanded") === 'false' && window.getComputedStyle(expandEl).display === 'block') {
+			expandedOpen(btn, expandEl);
+		}
+	});
+}
+function expandedClose(btn, expandEl) {
+	btn.setAttribute("aria-expanded", false);
+}
+function expandedOpen(btn, expandEl) {
+	btn.setAttribute("aria-expanded", true);
+}
