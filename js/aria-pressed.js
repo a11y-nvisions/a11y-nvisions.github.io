7d05e903c356e1a9981e62f3a91dee994d3886e3
Hyongsop Kim <hyongsop.kim@nvisions.co.kr>
2021-12-31 15:11:44 +0900

    aria-pressed.js 톡백에서 이중탭할 때 켜짐, 꺼짐 발화하도록 기능 추가.

--- js/aria-pressed.js
+++ js/aria-pressed.js
@@ -19,6 +19,25 @@ try {
   document.getElementsByTagName("head")[0].appendChild(jScript);
 }
 
+ function announceForAccessibility(message) {
+  var html = '' +
+    '<div aria-live="polite" name="div_announceForAccessibility" style="border: 0; padding: 0; margin: 0; ' +
+    'position: absolute !important;' + 'height: 1px; width: 1px; overflow: hidden; clip: rect(1px 1px 1px 1px); ' +
+    'clip: rect(1px, 1px, 1px, 1px);' + 'clip-path: inset(50%); white-space: nowrap;">' +
+    '<p name="p_announceForAccessibility"></p></div>';
+
+  $("body").append(html); // body 끝에 div_announceForAccessibility 추가
+
+  setTimeout(function () { // 0.02초 후 p 태그에 message 추가
+    $("[name='p_announceForAccessibility']").text(message);
+  }, 200);
+  setTimeout(removeAnnounceForAccessibility, 500); // 0.5초 후 div_announceForAccessibility 삭제
+}
+
+function removeAnnounceForAccessibility() {
+  $("[name='div_announceForAccessibility']").remove();
+}
+
 // DOM 을 미리 로드시켜 라이브러리를 정상적으로 작동시키기 위함
 window.addEventListener('load', function() {
 
@@ -27,31 +46,45 @@ window.addEventListener('load', function() {
    * DOM의 모든 버튼 (<Button>, <input type="button">,<a role="button"> 등) 클릭 시 발생하는 이벤트 (누름 상태 변경)
    * aria-pressed = "true" or "false"
    */
-  var beforeOuterHtml; // 클릭한 버튼의 outerHtml
+  var beforeOuterHtml;
   var ua = navigator.userAgent.toLowerCase();
   var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
-  if (isAndroid) {
     $(document).on("mousedown", ":button[aria-pressed], [type='button'][aria-pressed], [role='button'][aria-pressed]", function (e) {
       if ($(this).attr("aria-pressed") === undefined) { return; } 
       beforeOuterHtml = this.outerHTML;
     });
-  } else {
     $(document).on("focus", ":button[aria-pressed], [type='button'][aria-pressed], [role='button'][aria-pressed]", function (e) {
       if ($(this).attr("aria-pressed") === undefined) { return; }
       beforeOuterHtml = this.outerHTML;
     });
-  }
 
   $(document).on("click", ":button[aria-pressed], [type='button'][aria-pressed], [role='button'][aria-pressed]", function (e) {
-	var $this = $(this); // 클릭한 요소의 Object
-	var _this = this; // 클릭한 요소의 태그 요소
+	var $this = $(this);
+	var _this = this;
 	var timeout = setTimeout(function() {
-		if (beforeOuterHtml === _this.outerHTML){ return; }
-		if ($this.attr("aria-pressed") === "true") { // aria-pressed 가 true(누름 상태)면 false 로 변경
-		  $this.attr("aria-pressed", "false");
-		} else { // aria-pressed 가 undefined 거나 false 면 true 로 변경
+    if (isAndroid) {
+      if (beforeOuterHtml === _this.outerHTML) { 
+         $this.attr("aria-disabled", "true");
+      } else if ($this.attr("aria-pressed") === "true") {
+        $this.removeAttr("aria-disabled");
+        $this.attr("aria-pressed", "false");
+        announceForAccessibility("꺼짐");
+      } else {
+        $this.removeAttr("aria-disabled");
+        $this.attr("aria-pressed", "true");
+        announceForAccessibility("켜짐");
+    }; 
+  } else {
+		if (beforeOuterHtml === _this.outerHTML){ 
+      $this.attr("aria-disabled", "true");  
+    } else if ($this.attr("aria-pressed") === "true") {
+      $this.removeAttr("aria-disabled");
+  $this.attr("aria-pressed", "false");
+		} else {
+      $this.removeAttr("aria-disabled");
 		  $this.attr("aria-pressed", "true");
-  }
+  };
+    };
 	}, 500);
 
   });
